821ca040985bf7bc61259f06f0281fe9a97f7d12
Adam Solchenberger <asolchenberger@gmail.com>
2024-06-26 21:33:15 -0500

    converted more pngs to svgs

--- SVGs/make_disa.py
+++ SVGs/make_disa.py
@@ -1,11 +1,15 @@
 
 GRAY_30 = "#b3b3b3"
 GRAY_50 = "#808080"
-
-f_name = input("Enter file name:")
-
-with open(f"{f_name}.svg", "r") as fp:
-    text = fp.read()
-
-with open(f"{f_name}_Disa.svg", "w") as fp:
-    fp.write(text.replace(GRAY_50, GRAY_30))
+f_name = "BLAH"
+while (len(f_name)):
+    f_name = input("Enter file name:")
+    try:
+        with open(f"{f_name}.svg", "r") as fp:
+            text = fp.read()
+        with open(f"{f_name}_Disa.svg", "w") as fp:
+            fp.write(text.replace(GRAY_50, GRAY_30))
+        print(f"{f_name}_Disa.svg created")
+    except Exception as e:
+        print(f"{e}")
+        print(f"Try again :(")
